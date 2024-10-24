19202a7f43071abf9ba13a83e46b9c7e3e6f7ab9
sjbtherapy247 <sjbtherapy247@gmail.com>
2024-10-24 17:43:31 +1100

    new site commit

---
+++ config/seo.js
@@ -0,0 +1,21 @@
+export const defaultSeoConfig = {
+    title: 'Tranceformd - Transform Your Mind with Hypnosis',
+    description: 'Empower yourself with clinical hypnotherapy. Book a session with Simon Baker to remove anxiety, stress, or fears.',
+    canonical: 'https://tranceformd.com',
+    openGraph: {
+      type: 'website',
+      locale: 'en_US',
+      url: 'https://tranceformd.com',
+      title: 'Tranceformd - Transform Your Mind with Hypnosis',
+      description: 'Empower yourself with clinical hypnotherapy.',
+      images: [
+        {
+          url: 'https://tranceformd.com/assets/images/cover-image.jpg',
+          width: 1200,
+          height: 630,
+          alt: 'Tranceformd Cover Image',
+        },
+      ],
+    },
+  };
+  
--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
-// ----------------------------------------------------------------------
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+});
 
-module.exports = {
-  trailingSlash: true,
-};
+module.exports = withMDX({
+  pageExtensions: ['js', 'jsx', 'md', 'mdx'],
+});
